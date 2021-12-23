10c7e76e524649a2e8262cf7351c2e1ec9c3ea30
Tarif Halabi <tarif.halabi@gmail.com>
2021-12-23 11:48:58 -0500

    .

--- hangar-iot-be/src/test/java/com/kerneldc/mqttpublisher/MqttpublisherApplicationTests.java
+++ hangar-iot-be/src/test/java/com/kerneldc/mqttpublisher/MqttpublisherApplicationTests.java
@@ -2,11 +2,9 @@ package com.kerneldc.mqttpublisher;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
-@ActiveProfiles("test")
+//@SpringBootTest
+//@ActiveProfiles("test")
 class MqttpublisherApplicationTests {
 
 	@Disabled
