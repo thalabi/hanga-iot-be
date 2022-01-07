19d89ce3cdaa370fb630dd2a5d9c8ea768adacc3
Tarif Halabi <tarif.halabi@gmail.com>
2022-01-06 20:39:41 -0500

    Add more attributes.

--- hangar-iot-be/src/main/java/com/kerneldc/hangariot/controller/Device.java
+++ hangar-iot-be/src/main/java/com/kerneldc/hangariot/controller/Device.java
@@ -5,7 +5,13 @@ import lombok.Data;
 @Data
 public class Device {
 
+	private enum DeviceTypeEnum {PLUG}
+	
 	private String name;
 	private String description;
-
+	private String location;
+	private DeviceTypeEnum deviceType;
+	private Boolean telemetry;
+	private String iotDeviceMake;
+	private String iotDeviceModel;
 }
